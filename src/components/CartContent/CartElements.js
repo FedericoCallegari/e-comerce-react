c88293d9eb1db3ae4488c848624196801b58601e
Federico Callegari <callegarifede3@gmail.com>
2024-08-01 18:14:46 -0300

    Add erase products function

--- src/components/CartContent/CartElements.js
+++ src/components/CartContent/CartElements.js
@@ -7,7 +7,17 @@ import React from 'react'
 
 const CartElements = () => {
 
-    const {cart} = useContext(dataContext);
+    const {cart, setCart} = useContext(dataContext);
+
+    const deleteProduct = (id)=>{
+        const foundId = cart.find((element)=> element.id === id)
+
+        const newCart = cart.filter((element)=>{
+            return element !== foundId;
+        });
+        setCart(newCart);
+    };
+
     return cart.map((product)=>{
         return(
             <div className="cartContent" key={product.id}>
@@ -15,6 +25,7 @@ const CartElements = () => {
                 <h3 className="name" >{product.name}</h3>
                 <CartItemCounter product={product}/>
                 <h4 className="price">{product.price * product.quanty}$</h4>
+                <h3 className="cart-delete-button" onClick={ ()=> deleteProduct(product.id)}>❌</h3>
             </div>
         )
     })
