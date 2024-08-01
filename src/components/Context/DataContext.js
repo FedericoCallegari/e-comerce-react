1663f0928e26de2431157718c53d7fbc8457947b
Federico Callegari <callegarifede3@gmail.com>
2024-08-01 19:09:23 -0300

    Add navbar in my cart content

--- src/components/Context/DataContext.js
+++ src/components/Context/DataContext.js
@@ -1,15 +1,12 @@
-import { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import { createContext, useState } from "react";
+
 
 export const dataContext = createContext();
 
 const DataProvider = ({ children }) => {
-  const [data, setData] = useState([]);
   const [cart, setCart] = useState([]);
 
-  useEffect(() => {
-    axios("data.json").then((res) => setData(res.data));
-  }, []);
+  
 
   const buyProducts = (product) => {
     const productRepeat = cart.find((item) => item.id === product.id);
@@ -27,7 +24,7 @@ const DataProvider = ({ children }) => {
   };
 
   return (
-    <dataContext.Provider value={{ data, cart, setCart, buyProducts }}>
+    <dataContext.Provider value={{ cart, setCart, buyProducts }}>
       {children}
     </dataContext.Provider>
   );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,10 +1,16 @@
-import { useContext } from "react";
+import { useContext ,useState, useEffect} from "react";
 import { dataContext } from "../Context/DataContext";
+import axios from "axios";
 import "./Products.css";
 
 const Products = () => {  
-    const { data, buyProducts } = useContext(dataContext);
+    
 
+    const [data, setData] = useState([]);
+    const { buyProducts } = useContext(dataContext);
+    useEffect(() => {
+      axios("data.json").then((res) => setData(res.data));
+    }, []);
   
 
   return data.map((product)=>{
@@ -15,7 +21,7 @@ const Products = () => {
             <h4>{product.price}$</h4>
             <button onClick={()=>buyProducts(product)}>Comprar</button>
         </div>
-    )
+    ) 
   })
 }
 
