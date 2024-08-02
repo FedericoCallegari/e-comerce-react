5d0353e4f53c1bf38ba69a8befd7568517db21f6
Federico Callegari <callegarifede3@gmail.com>
2024-08-01 23:23:12 -0300

    Add localStorage in my app

--- src/components/Context/DataContext.js
+++ src/components/Context/DataContext.js
@@ -1,13 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 export const dataContext = createContext();
 
 const DataProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const savedCart = JSON.parse(localStorage.getItem("cart")) || [];
+  const [cart, setCart] = useState(savedCart);
 
+  useEffect(()=>{
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
   
-
+  //setItem
+  //getItem
   const buyProducts = (product) => {
     const productRepeat = cart.find((item) => item.id === product.id);
     if (productRepeat) {
